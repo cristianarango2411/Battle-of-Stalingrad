d2e2bf3d202a6caf347a20155fc9c864fa43d53d
Cristian Arango <cristian.arango1910@gmail.com>
2024-03-28 17:41:41 -0500

    Tank properties

--- src/Model/Tank.php
+++ src/Model/Tank.php
@@ -8,15 +8,28 @@ class Tank {
     public $speed; 
     public $fuelRange;
     public $position;
+    public $turretRange;
+    public $viewport;
+    public $barrel;
+    public $turretRing;
+    public $commanderHatch;
+    public $wheels;
 
 
-    public function __construct($name, $health, $attack, $defense, $speed, $fuelRange, $x, $y) {
+
+    public function __construct($name, $health, $attack, $defense, $speed, $fuelRange, $turretRange, $viewport, $barrel, $turretRing, $commanderHatch, $wheels, $x, $y) {
         $this->attack = $attack;
         $this->defense = $defense;
         $this->name = $name;
         $this->health = $health;
         $this ->speed = $speed;
         $this ->fuelRange = $fuelRange;
+        $this ->turretRange = $turretRange;
+        $this ->viewport = $viewport;
+        $this ->barrel = $barrel;
+        $this ->turretRing = $turretRing;
+        $this ->commanderHatch = $commanderHatch;
+        $this ->wheels = $wheels;
         $this->position = ['x' => $x, 'y' => $y];
     }
 
@@ -62,6 +75,54 @@ class Tank {
 
     public function setfuelRange($fuelRange) {
         $this->fuelRange = $fuelRange;
+    }
+
+    public function getturretRange() {
+        return $this->turretRange;
+    }
+
+    public function setturretRange($turretRange) {
+        $this->turretRange = $turretRange;
+    }
+
+    public function getviewport() {
+        return $this->viewport;
+    }
+
+    public function setviewport($viewport) {
+        $this->viewport = $viewport;
+    }
+
+    public function getbarrel() {
+        return $this->barrel;
+    }
+
+    public function setbarrel($barrel) {
+        $this->barrel = $barrel;
+    }
+
+    public function getturretRing() {
+        return $this->turretRing;
+    }
+
+    public function setturretRing($turretRing) {
+        $this->turretRing = $turretRing;
+    }
+
+    public function getcommanderHatch() {
+        return $this->commanderHatch;
+    }
+
+    public function setcommanderHatch($commanderHatch) {
+        $this->commanderHatch = $commanderHatch;
+    }
+
+    public function getwheels() {
+        return $this->wheels;
+    }
+
+    public function setwheels($wheels) {
+        $this->wheels = $wheels;
     }
 
     public function getPosition()
