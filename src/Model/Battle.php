c83d9b7c5e7ed8b846a8c05dadb643db9c4a8fce
Cristian Arango <cristian.arango1910@gmail.com>
2024-04-01 01:19:11 -0500

    clean folders

--- src/Model/Battle.php
+++ src/Model/Battle.php
@@ -106,12 +106,4 @@ class Battle
         }
         return $tanksInRange;
     }
-
-    private function isInRange(Tank $tank1, Tank $tank2)
-    {
-        $position1 = $tank1->getPosition();
-        $position2 = $tank2->getPosition();
-        $distance = sqrt(pow($position1['x'] - $position2['x'], 2) + pow($position1['y'] - $position2['y'], 2));
-        return $distance <= $tank1->getturretRange();
-    }
 }
